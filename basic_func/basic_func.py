3866ba1f277946e947e83db780f5da472b2f84be
JR <yjr6197@gmail.com>
2025-09-02 11:35:56 +0900

    submit

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,41 @@
 def add(a, b):
-    pass
+    c = a + b
+    return c
 
 
 def sub(a, b):
-    pass
+    c = a - b
+    return c
 
 
 def mul(a, b):
-    pass
-
+    c = a * b
+    return c
+    
+    
 
 def div(a, b):
-    pass
+    c = a // b
+    return c
 
 
 def power(base, pow):
-    pass
+    c = base**pow
+    return c
 
 
 def square(base):
-    pass
+    c = base * base
+    return c
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    if 이름 == "낯선자" and 나이 == 20:
+        return "안녕하신가 낯선자!"
+    elif 이름 == "마법사": 
+        return "안녕하십니까 마법사!"
+    elif 나이 == 4:
+        return "안녕 낯선자!"
+    
+     
+    
